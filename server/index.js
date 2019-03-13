00bff2391e68d445259e4dbdeae653fde8ac6a40
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-03-13 13:44:23 -0400

    receiving data from mta

--- server/index.js
+++ server/index.js
@@ -2,20 +2,40 @@ const express = require('express')
 const app = express()
 const compression = require('compression')
 const helmet = require('helmet')
+const request = require('request')
+const GtfsRealtimeBindings = require('gtfs-realtime-bindings')
+require('dotenv').config()
 
 app.use(compression())
 app.use(helmet())
 
+// get subway times from mta and deliver to frontend
+app.get('/refresh', async (req, res, next) => {
+  request(
+    {
+      method: 'GET',
+      url: `http://datamine.mta.info/mta_esi.php?key=${
+        process.env.MTA_KEY
+      }&feed_id=1`,
+      encoding: null
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        var feed = GtfsRealtimeBindings.FeedMessage.decode(body)
+        console.log(feed.entity[0].trip_update.stop_time_update)
+      }
+    }
+  )
+})
+
 // home route
 app.get('/', (req, res, next) => {
   res.json({ message: 'Welcome to the API' })
-  next()
 })
 
 // 404 route
 app.get('/*', (req, res, next) => {
   res.status(404).json({ message: 'Not Found' })
-  next()
 })
 
 // set port
